e4e7f3284a349bc6f9f51c45088b5c4438b47284
Damian Nowakowski <damiano@Air-Damiano.local>
2026-04-11 16:52:46 +0100

    1736. Latest time by replacing hidden digits

---
+++ 1736. Latest Time by Replacing Hidden Digits.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/latest-time-by-replacing-hidden-digits/description/
+class Solution {
+public:
+    string maximumTime(string time) {
+
+        if (time[0] == '?' && time[1] == '?') {
+            time[0] = '2';
+            time[1] = '3';
+        } else if (time[0] == '?' && time[1] < '4') {
+            time[0] = '2';
+        } else if (time[0] == '?' && time[1] >= '4') {
+            time[0] = '1';
+        }
+        if (time[1] == '?' && time[0] == '2') {
+            time[1] = '3';
+        } else if (time[1] == '?') {
+            time[1] = '9';
+        }
+
+        if (time[3] == '?') {
+            time[3] = '5';
+        }
+        if (time[4] == '?') {
+            time[4] = '9';
+        }
+
+        return time;
+    }
+};
