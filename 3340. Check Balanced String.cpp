86da347140395d1e8b9cebed50b6a34977bd594c
Damian Nowakowski <damienko0906@gmail.com>
2026-04-17 15:35:33 +0100

    3349, check balanced string

---
+++ 3340. Check Balanced String.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/check-balanced-string/description/
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int odd = 0;
+        int even = 0;
+        for(int i=0; i<num.size(); i++){
+            if(i % 2 == 0){
+                even += num[i] - '0';
+            } else {
+                odd += num[i] - '0';
+            }
+        }
+
+        return odd == even;
+    }
+};
