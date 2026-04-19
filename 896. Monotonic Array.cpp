bf616a7762fd721eaa9fec7336bfa7fc9d400cd2
Damian Nowakowski <damienko0906@gmail.com>
2026-04-19 14:15:28 +0100

    896, monotonic array

---
+++ 896. Monotonic Array.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/monotonic-array/description/
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return true;
+
+        bool isInc = true;
+        bool isDec = true;
+
+        for(int i=1; i<n; i++){
+            if(!isInc && !isDec){
+                return false;
+            }
+
+            if(nums[i] < nums[i-1]){
+                isInc = false;
+            }
+
+            if(nums[i] > nums[i-1]){
+                isDec = false;
+            }
+        }
+
+        return isDec || isInc;
+    }
+};
