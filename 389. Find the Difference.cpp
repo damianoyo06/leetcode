076a023549e1c6a0fbf565dea5e51689a6407286
Damian Nowakowski <damienko0906@gmail.com>
2026-04-23 12:01:00 +0100

    389. find the difference

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/find-the-difference/description/
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        
+        for (char c : t) sum += c;
+        for (char c : s) sum -= c;
+        
+        return (char)sum;
+    }
+};
