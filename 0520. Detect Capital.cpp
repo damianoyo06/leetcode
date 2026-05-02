6538f1fc63b82b98f477c21ff6916629a9667414
Damian Nowakowski <damienko0906@gmail.com>
2026-05-02 19:25:52 +0100

    detect capital

---
+++ 0520. Detect Capital.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/detect-capital/description/
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for(char c:word){
+            if(isupper(c)){
+                count++;
+            }
+        }
+
+        if(count == word.size()) return true;
+        if(count == 0) return true;
+        if(count == 1 && isupper(word[0])) return true;
+
+        return false;
+    }
+};
