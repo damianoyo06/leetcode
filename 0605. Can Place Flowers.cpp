b7e14ec7eed7d3cb3beaa29614a72a40300736bc
Damian Nowakowski <damienko0906@gmail.com>
2026-05-12 15:12:26 +0100

    Flower Bed

---
+++ 0605. Can Place Flowers.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/can-place-flowers/description/
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+        for(int i = 0; i < flowerbed.size(); i++) {
+
+            bool leftEmpty =
+                (i == 0 || flowerbed[i - 1] == 0);
+
+            bool rightEmpty =
+                (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0);
+
+            if(flowerbed[i] == 0 && leftEmpty && rightEmpty) {
+                flowerbed[i] = 1;
+                n--;
+            }
+
+            if(n <= 0)
+                return true;
+        }
+
+        return n <= 0;
+    }
+};
