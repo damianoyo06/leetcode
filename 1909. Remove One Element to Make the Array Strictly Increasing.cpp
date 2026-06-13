3d00bc2615c7c3214d2b92d06bd5258250ea5129
Damian Nowakowski <damienko0906@gmail.com>
2026-06-13 19:48:20 +0100

    remove one element

---
+++ 1909. Remove One Element to Make the Array Strictly Increasing.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        int removed = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                removed++;
+
+                if (removed > 1) return false;
+
+                if (i > 1 && nums[i] <= nums[i - 2]) {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+
+        return true;
+    }
+};
